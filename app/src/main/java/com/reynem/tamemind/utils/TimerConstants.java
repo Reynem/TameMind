66f819885a93e28771f31bba38931dabd0fdedb9
Kerim_Lex <lexmeins1@gmail.com>
2025-06-08 16:09:01 +0500

    Constants

---
+++ app/src/main/java/com/reynem/tamemind/utils/TimerConstants.java
@@ -0,0 +1,12 @@
+package com.reynem.tamemind.utils;
+
+public class TimerConstants {
+    public static final String TIMER_NOTIFICATION_CHANNEL_ID = "dynamic_timer_channel_v2";
+    public static final int TIMER_NOTIFICATION_ID = 4; // ID for notifications
+    public static final String PREFS_NAME = "app_prefs";
+    public static final String PREF_PROGRESS_SECONDS = "timer_progress_seconds";
+    public static final String PREF_IS_TIMER_ACTIVE = "timer_is_timer_active";
+    public static final String PREF_BLOCK_UNTIL = "block_until";
+    public static final String PREF_GET_ALL_TIME = "get_all_time";
+    public static final String PREF_LAST_TIMER_VALUE = "last_timer_value";
+}
